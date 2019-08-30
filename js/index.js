729d7ff691052f5baffd2036603ef404346e519b
innakondur <innakondur@gmail.com>
2019-08-30 17:13:56 +0000

    try

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let message = 'Hello JavaScript';
+alert(message);
+message = 'Nice to see you!';
+alert(message);
+
+let userAge = parseInt(prompt('Enter your age'));
+alert('your  age is ' + userAge)
+
+let nextAge = userAge + 1;
+alert('next yar you will be' + nextAge)
